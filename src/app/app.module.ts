a85fa53bf1d7f0616a463c94538d315616642304
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-10-26 10:15:10 +0700

    add startgame component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { StartgameComponent } from './components/startgame/startgame.component';
+import { MainComponent } from './components/main/main.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'startgame', component: StartgameComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    StartgameComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
