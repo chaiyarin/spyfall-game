1bd85ed7c0890760990acfdd6efce366f9d174b3
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-10-26 11:10:00 +0700

    add bootstrap and font awesome and add playroom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { StartgameComponent } from './components/startgame/startgame.component';
 import { MainComponent } from './components/main/main.component';
+import { PlayroomComponent } from './components/playroom/playroom.component';
 
 const appRoutes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'startgame', component: StartgameComponent }
+  { path: 'startgame', component: StartgameComponent },
+  { path: 'playroom/:memberName/:timePerRound', component: PlayroomComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    StartgameComponent
+    StartgameComponent,
+    PlayroomComponent
   ],
   imports: [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/playroom/playroom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playroom',
+  templateUrl: './playroom.component.html',
+  styleUrls: ['./playroom.component.css']
+})
+export class PlayroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
